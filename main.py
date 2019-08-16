3deb2148d94854e961d85e72bff98309c79d9101
federico.marsiglia <federico.marsiglia@kalpa.it>
2019-08-16 16:34:07 +0200

    WIP

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     for i in range(1, len(sys.argv)):
 
         toInstrument = sys.argv[i]
-        #print('STO PARSANDO ====> ' + toInstrument)
+        print('STO PARSANDO ====> ' + toInstrument)
         instrumenter.Instrument(toInstrument)
