a2d552754ded43425ce3826e09037a9613cb7518
federico.marsiglia <federico.marsiglia@kalpa.it>
2020-01-29 17:17:04 +0100

    fix

--- instrument.sh
+++ instrument.sh
@@ -1,13 +1,3 @@
-python="python"
-if ! [ -x "$(command -v python)" ]; then
-  echo 'Python is not installed. Trying python3'
-fi
-
-python="python3"
-if ! [ -x "$(command -v python3)" ]; then
-  echo 'Python is not installed.'
-fi
-
 orig_dir="$PWD"
 directory="$1" 
 pattern='.*\.cs'
@@ -17,16 +7,20 @@ cd $directory
 git reset HEAD --hard
 find . -type d -iname "bin" -o -iname "obj" | xargs rm -rf
 
-$python $orig_dir/main.py `find ./Mobile* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./MTS* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Ip* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Web* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Communication -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./MQTT* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Widgets* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Nano* -regextype grep -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./Zeroconf* -type f -iregex $pattern`
-$python $orig_dir/main.py `find ./ANR* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./MTS* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./Ip* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./Web* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./Communication -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./MQTT* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./Nano* -regextype grep -type f -iregex $pattern`
+python $orig_dir/main.py `find ./Zeroconf* -type f -iregex $pattern`
+python $orig_dir/main.py `find ./ByMe_Lib -regextype grep -type f -iregex $pattern`
+
+for d in  `find ./WidgetsLib_ -maxdepth 1 -type d`
+do 
+	python $orig_dir/main.py `find $d -regextype grep -type f -iregex $pattern`
+done
+
 
 git checkout `find -type f -iname 'logbroker\.cs'`  
 git checkout `find -type f -iname 'fourthphase\.xaml\.cs'`  
