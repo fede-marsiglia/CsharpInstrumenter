a00a2e48a3095a6713e98e89a33dc1b1b1f13cba
federico marsiglia <federico.marsiglia@kalpa.it>
2020-04-26 16:15:52 +0200

    fixes

--- main.py
+++ main.py
@@ -7,39 +7,42 @@ class Instrumenter:
 
         self._methodStartPattern = r'\w+[ ]*(\<.*?\>)?[ ]+\w+[ ]*\(([\[\]a-zA-Z1-9_,.=<>\"\s ]+)?\)\s*\{'
         self._stopWatchStart = "\n\t\t\tvar stopWatch = new System.Diagnostics.Stopwatch(); \n\t\t\t stopWatch.Start();"
-        self._stopWatchStop = "\n\t\t{\n\t\t\tstopWatch.Stop(); \n\t\t\tvar ts = stopWatch.Elapsed;"
-        self._elapsedTime = "\n\t\t\tvar elapsedTime = System.String.Format(\"{0:00}:{1:00}:{2:00}.{3:00}\", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);"
-        self._logTimeSpent = "\n\t\t\tComm.Log.LogBroker.Instance.TraceDebug(\"Runtime =>\" + elapsedTime);\n\t\t"
+        self._elapsedTime = "\n\t\t\tSystem.String.Format(\"{0:00}:{1:00}:{2:00}.{3:00}\", stopWatch.Elapsed.Hours, stopWatch.Elapsed.Minutes, stopWatch.Elapsed.Seconds)"
+        self._logTimeSpent = "\n\t\t\tComm.Log.LogBroker.Instance.TraceDebug(\"Runtime =>\" + " + self._elapsedTime + ");\n\t\t"
         self._instrumentedFileContent = ''
 
     def GetFuncExitPoints(self, methodStartToFileEnd):
 
+        ret = []
         lastClsd = self.FindMethodClosingBrace(methodStartToFileEnd)
+
         retRx = re.compile('\sreturn(;|\s)')
         match = retRx.finditer(methodStartToFileEnd[:lastClsd])
 
-        ret = []
         for x in retRx.finditer(methodStartToFileEnd[:lastClsd]):
                 ret.append(x.span()[0])
 
-        if len(ret) == 0:
-            ret.append(lastClsd)
+        ret.append(lastClsd)
 
         return ret
 
-    def FindMethodClosingBrace(self, fileContent):
+    def FindMethodClosingBrace(self, methodStartToEOF):
 
         open = 1
         lastOpen = -1
         lastClsd = -1
 
+        searchForOpen = True;
+
         while open > 0:
 
-            foundOpen = fileContent.find('{', lastOpen + 1)
-            foundClose = fileContent.find('}', lastClsd + 1)
+            foundOpen = methodStartToEOF.find('{', lastOpen + 1)
+            foundClose = methodStartToEOF.find('}', lastClsd + 1)
 
-            if foundClose >= 0 and foundOpen >= 0:
+            if foundOpen < 0:
+                searchForOpen = False
 
+            if searchForOpen:
                 if foundOpen < foundClose:
                     open += 1
                     lastOpen = foundOpen
@@ -47,7 +50,8 @@ class Instrumenter:
                     open -= 1
                     lastClsd = foundClose
             else:
-                break
+                open -= 1
+                lastClsd = foundClose
 
         return lastClsd
 
@@ -80,8 +84,6 @@ class Instrumenter:
                 for x in funcExitPoints:
 
                     self._instrumentedFileContent += fileContent[prev : x]
-                    self._instrumentedFileContent += self._stopWatchStop
-                    self._instrumentedFileContent += self._elapsedTime
                     self._instrumentedFileContent += self._logTimeSpent
                     prev = x
 
